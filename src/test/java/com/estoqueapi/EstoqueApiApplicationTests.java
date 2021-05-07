4047928ef2e8d1d8364d8ef5c1b75e401bfa7364
unknown <edvan.reis.moura@gmail.com>
2021-05-07 14:12:11 -0300

    ajuste dos nome do projeto

--- src/test/java/com/estoqueapi/EstoqueApiApplicationTests.java
+++ src/test/java/com/estoqueapi/EstoqueApiApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import com.estoqueapi.service.EstoqueService;
 
 @SpringBootTest
-class GftapiApplicationTests {
+class EstoqueApiApplicationTests {
 
 	@Autowired
 	EstoqueService service;
