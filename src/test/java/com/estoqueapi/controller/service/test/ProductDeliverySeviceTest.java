f60f24eed9fe14ae7bef0e2becd42a61fe0d6899
unknown <edvan,reis.moura@gmail.com>
2021-06-08 09:10:10 -0300

    melhorando teste controller

--- src/test/java/com/estoqueapi/controller/service/test/ProductDeliverySeviceTest.java
+++ src/test/java/com/estoqueapi/controller/service/test/ProductDeliverySeviceTest.java
@@ -1,28 +1,42 @@
 package com.estoqueapi.controller.service.test;
 
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.springframework.context.MessageSource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.estoqueapi.exception.EstoqueApiException;
-import com.estoqueapi.service.ProductDeliverySevice;
+import com.estoqueapi.service.EstoqueService;
+import com.estoqueapi.service.impl.ProductDeliverySeviceImpl;
 
 @DisplayName("Tests for the EstoqueServiceTest ")
 @ExtendWith(SpringExtension.class)
 public class ProductDeliverySeviceTest {
 
+	@InjectMocks
+	private ProductDeliverySeviceImpl productDeliverySevice;
+	
 	@Mock
-	private ProductDeliverySevice productDeliverySevice;
+	private EstoqueService repository;
+	
+	@Mock
+	private  MessageSource messageSource;
 	
 	
 	@Test
 	void getInventoryByStoreTest() {
 		try {
-			this.productDeliverySevice.getInventoryByStore(Mockito.anyString(), Mockito.anyInt());
+			when(this.repository.findByProduct("EMS")).thenReturn(new ArrayList<>());
+			this.productDeliverySevice.getInventoryByStore("EMS",2);
 		} catch (EstoqueApiException e) {
 			Assertions.assertNotNull(e);
 		}
